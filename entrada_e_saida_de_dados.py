cd7370d8b12793e6b3bd57bff5408f855af7b1bc
Jeferson Caye <jefercaye@yahoo.com.br>
2026-01-16 18:58:24 -0300

    Aula 8 Entrada e saída de dados

---
+++ entrada_e_saida_de_dados.py
@@ -0,0 +1,42 @@
+"""
+Aula: Entrada e saída de dados
+"""
+
+# ================================
+# SAÍDA DE DADOS
+# ================================
+
+print("Início da aula de entrada e saída de dados")
+print("Python para QAs – Fundamentos à Prática")
+print(100)
+print(2.5)
+
+print("\n---\n")
+
+# ================================
+# ENTRADA DE DADOS
+# ================================
+
+nome = input("Digite seu nome: ")
+print("Nome informado:", nome)
+
+print("\n---\n")
+
+idade = input("Digite sua idade: ")
+print("Idade informada:", idade)
+print("Tipo da variável idade:", type(idade))
+
+print("\n---\n")
+
+# ================================
+# USANDO F-STRING
+# ================================
+
+cidade = input("Digite sua cidade: ")
+
+print(f"Usuário: {nome}")
+print(f"Cidade: {cidade}")
+
+print("\n---\n")
+
+print("Fim do script")
